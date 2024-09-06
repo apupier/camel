1ed7d0a3ca97cc5e8b553a4071b0aa5ed33205e4
Aurélien Pupier <apupier@redhat.com>
2024-09-06 14:47:32 +0200

    WIP CAMEL-20593 - Use Apache kafka container image for tests
    - Different Class for TestContainer to be used, the previously used was
    deprecated https://java.testcontainers.org/modules/kafka/
    - Zookeeper is no more available
    
    Currently all idempotent tests are failing with
    org.apache.camel.FailedToStartRouteException: Failed to start route
    route1 because of java.lang.IllegalArgumentException: Target offsets
    must be non-empty
    	at org.apache.camel.impl.engine.RouteService.warmUp(RouteService.java:125)
    
    Signed-off-by: Aurélien Pupier <apupier@redhat.com>

--- test-infra/camel-test-infra-kafka/src/test/java/org/apache/camel/test/infra/kafka/services/ContainerLocalAuthKafkaService.java
+++ test-infra/camel-test-infra-kafka/src/test/java/org/apache/camel/test/infra/kafka/services/ContainerLocalAuthKafkaService.java
@@ -21,7 +21,7 @@ import org.apache.camel.test.infra.common.services.ContainerService;
 import org.apache.camel.test.infra.kafka.common.KafkaProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.kafka.KafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 import org.testcontainers.utility.MountableFile;
 
@@ -36,7 +36,7 @@ public class ContainerLocalAuthKafkaService implements KafkaService, ContainerSe
                     ContainerLocalKafkaService.KAFKA3_IMAGE_NAME))
                     .asCompatibleSubstituteFor(ContainerLocalKafkaService.KAFKA3_IMAGE_NAME));
 
-            withEmbeddedZookeeper();
+            //withEmbeddedZookeeper();
 
             final MountableFile mountableFile = MountableFile.forClasspathResource(jaasConfigFile);
             LOG.debug("Using mountable file at: {}", mountableFile.getFilesystemPath());
